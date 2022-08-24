7d87fbbd0f0a06f106074d8da68cf789216cdd6e
Zahra Madineh <madinehzahra76@gmail.com>
2022-08-24 22:08:05 +0430

    edit structure

--- js/main-page.js
+++ js/main-page.js
@@ -1,7 +1,18 @@
 const hamburger = document.querySelector(".mp-hamburger");
 const navMneu = document.querySelector(".mp-nav-menu");
+const sighUpNewText = document.getElementById("sighUp-child")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMneu.classList.toggle("active");
+    sighUpNewText.classList.toggle("active");
+    
+})
+
+const list = document.getElementById("list");
+const listButton = document.querySelector(".mp-open-list-button");
+
+listButton.addEventListener("click", () => {
+        list.classList.toggle('open');
+        
 })
