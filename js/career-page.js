f548c3ec394d7c1e481644253869b7cbea6bfdf4
nima babaei <nimani1378@gmail.com>
2022-08-27 22:01:15 +0430

    adding slick library for carousel in career-page

---
+++ js/career-page.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.cp_imgSlider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
+  });
