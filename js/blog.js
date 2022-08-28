3e9187207d9aa566b0fcf1edb539176e2c6bb742
Amir partovi <a.partovi99@gmail.com>
2022-08-28 21:36:23 +0430

    'Blog - Dropdown - CardPosition'

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,9 @@
 function bl_search_Function() {
-    var x = document.getElementsById("bl-search-input");
+    var x = document.getElementById("bl-search-input");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
+    
   }
