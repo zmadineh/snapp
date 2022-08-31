adb99517a2d833994bd5be9ad07f13ed46d07023
Zahra Madineh <madinehzahra76@gmail.com>
2022-08-31 18:34:10 +0430

    change padding size of each section

--- js/main-page.js
+++ js/main-page.js
@@ -1,105 +1,65 @@
 const hamburger = document.querySelector(".mp-hamburger");
-const navMneu = document.querySelector(".mp-nav-menu");
-const sighUpNewText = document.getElementById("sighUp-child")
+const navMneu = document.querySelector(".mp-menu");
+const sighUpNewText = document.querySelector("#sighUp-child");
+const overlay = document.querySelector('.mp-overlay');
+const body = document.querySelector('body');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMneu.classList.toggle("active");
-    sighUpNewText.classList.toggle("active");
-
-    if(list.classList.contains('open'))
-        list.classList.remove('open');
-    
-});
-
-const list = document.getElementById("list");
+const list = document.querySelector("#list");
 const listButton = document.querySelector(".mp-d-app-links-title");
-const listButtonIcone = document.getElementById("open-close-icon")
+const listButtonIcone = document.querySelector("#open-close-icon");
 
-/* When the user clicks on the button,
-toggle between closing and opening the list content */
-listButton.addEventListener("click", () => {
-        list.classList.toggle('open');
-        listButtonIcone.classList.toggle('fa-angle-up');
-        
-});
+const inputData = document.querySelector("#mp-phone_input");
+const circleIcon = document.querySelector("#mp-circle-icon");
+const clearIcon = document.querySelector("#mp-clear-icon");
+const errorText = document.querySelector("#mp-error");
 
-const inputData = document.getElementById("mp-get-number");
-const circleIcon = document.getElementById("mp-circle-icon");
-const clearIcon = document.getElementById("mp-clear-icon");
-const dangerText = document.getElementById("mp-wrongNumber");
+const sendBtn = document.querySelector("#mp-send_btn");
 
-inputData.addEventListener('input', (e) => {
-        if(e.target.value != ''){
-                circleIcon.style.display = "none";
-                clearIcon.style.display = "flex";
+const signUpProcContainer = document.querySelector('#mp-signUpProc_container');
+
+function handleHamburgerMneu () {
+        hamburger.classList.toggle('active');
+        navMneu.classList.toggle('mobileView');
+        sighUpNewText.classList.toggle('active');
+        overlay.classList.toggle('active');
+        body.classList.toggle('hideScrollBar');
+
+        if(list.classList.contains('open')) {
+                list.classList.remove('open');
+                listButtonIcone.classList.toggle('fa-angle-down');
+                listButtonIcone.classList.toggle('fa-angle-up');
         }
-        else {
-                circleIcon.style.display = "flex";
-                clearIcon.style.display = "none";
-        }
-});
+
+        if(hamburger.classList.contains('active'))
+                scrollDisable();
+        else scrollEnable();
+}
 
 function clearInput () {
         inputData.value = "";
         circleIcon.style.display = "flex";
         clearIcon.style.display = "none";
-        dangerText.textContent = '';
+        errorText.textContent = '';
 }
-
-const sendLinkBtn = document.getElementById("send-link_btn");
-sendLinkBtn.addEventListener("click", () => {
-        const phoneNumberSyntax = /^09\d{9}/; 
-        
-        if (!inputData.value.match(phoneNumberSyntax)){
-                dangerText.textContent = "شماره موبایل وارد شده صحیح نیست";
-        }
-        else {
-                dangerText.textContent = '';
-        }
-});
-
-// Close the list menu if the user clicks outside of it
-// window.onclick = function(event) {
-//     if (!event.target.matches('.mp-hamburger') 
-//         && !event.target.matches('.mp-nav-menu') 
-//         && !event.target.matches('.mp-header-container')
-//         && !event.target.matches('.mp-bar')
-//         && !event.target.matches('.mp-d-app-links-list')) {
-//         if (navMneu.classList.contains('active')){
-//             navMneu.classList.remove('active')
-//         }
-//         if (list.classList.contains('open')){
-//             list.classList.remove('open')
-//         }
-//     }
-//   }
-
-const signUpProcContainer = document.getElementById('mp-signUpProc_container');
-signUpProcContainer.appendChild(create("../src/img/income.png", "درآمد تضمینی + پاداش های ماهانه و هفتگی", "با فعالیت در ناوگان اسنپ، علاوه بر کسب درآمد مستمر و امکان تسویه در لحظه می‌توانید با شرکت در طرح‌های تشویقی مختلف، درآمد خود را افزایش دهید."));
-signUpProcContainer.appendChild(create("../src/img/hour.png", "ساعت کاری دلخواه", "فعالیت در ناوگان اسنپ محدودیت زمانی ندارد و می توانید فعالیت خود را در هر ساعت از شبانه روز و متناسب با برنامه زندگی تان شخصی ساز کنید."));
-signUpProcContainer.appendChild(create("../src/img/benefits.png", "مزایا و خدمات باشگاه رانندگان", "در باشگاه رانندگان اسنپ می‌توانید از تسهیلات و خدمات متنوعی از جمله خدمات خودرویی، خدمات درمانی و همچنین خدمات رفاهی و آموزشی بهره‌مند شوید."));
-signUpProcContainer.appendChild(create("../src/img/carfix.png", "کارفیکس", "باشگاه رانندگان اسنپ به‌تازگی سرویس جدید «اسنپ کارفیکس» را برای سهولت دسترسی کاربران راننده به انواع خدمات خودرویی راه‌اندازی کرده است."));
-
 
 function create (img, title, body) {
         
         const mainContainer = document.createElement('div');
-        mainContainer.setAttribute('id', 'mp-procMainContainer');
+        mainContainer.className = 'mp-procMainContainer';
 
         const image = document.createElement("img");
-        image.setAttribute('id', 'mp-procImage');
+        image.className = 'mp-procImage';
         image.setAttribute('src', img);
 
         const container = document.createElement('div');
-        container.setAttribute('id', 'mp-procTextContainer');
+        container.className = 'mp-procTextContainer';
 
         const titleText =  document.createElement('h3');
-        titleText.setAttribute('id', 'mp-procText_h3');
+        titleText.className = 'mp-procText_h3';
         titleText.textContent = title;
 
         const botyText = document.createElement('p');
-        botyText.setAttribute('id', 'mp-procText_para');
+        botyText.className = 'mp-procText_para';
         botyText.textContent = body;
 
         container.appendChild(titleText);
@@ -110,3 +70,67 @@ function create (img, title, body) {
 
         return mainContainer;
 }
+
+function scrollDisable() {
+        // To get the scroll position of current webpage
+        TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+        LeftScroll = window.pageXOffset || document.documentElement.scrollLeft,
+        
+        // if scroll happens, set it to the previous value
+        window.onscroll = function() {
+                window.scrollTo(LeftScroll, TopScroll);
+        };
+}
+
+function scrollEnable() {
+        window.onscroll = function() {};
+}
+
+hamburger.addEventListener("click", () => {
+    handleHamburgerMneu();
+    
+});
+
+overlay.addEventListener('click', () => {
+        if (overlay.classList.contains('active'))
+                handleHamburgerMneu();
+});
+
+
+/* When the user clicks on the button,
+toggle between closing and opening the list content */
+listButton.addEventListener("click", () => {
+        list.classList.toggle('open');
+        listButtonIcone.classList.toggle('fa-angle-down');
+        listButtonIcone.classList.toggle('fa-angle-up');
+});
+
+inputData.addEventListener('input', (e) => {
+        if(e.target.value != ''){
+                circleIcon.style.display = "none";
+                clearIcon.style.display = "flex";
+        }
+        else {
+                circleIcon.style.display = "flex";
+                clearIcon.style.display = "none";
+        }
+        errorText.textContent = '';
+});
+
+sendBtn.addEventListener("click", () => {
+        const phoneNumberSyntax = /^09\d{9}/; 
+        
+        if (!inputData.value.match(phoneNumberSyntax)){
+                errorText.textContent = "شماره موبایل وارد شده صحیح نیست";
+        }
+        else {
+                errorText.textContent = '';
+        }
+});
+
+signUpProcContainer.appendChild(create("../src/img/income.png", "درآمد تضمینی + پاداش های ماهانه و هفتگی", "با فعالیت در ناوگان اسنپ، علاوه بر کسب درآمد مستمر و امکان تسویه در لحظه می‌توانید با شرکت در طرح‌های تشویقی مختلف، درآمد خود را افزایش دهید."));
+signUpProcContainer.appendChild(create("../src/img/hour.png", "ساعت کاری دلخواه", "فعالیت در ناوگان اسنپ محدودیت زمانی ندارد و می توانید فعالیت خود را در هر ساعت از شبانه روز و متناسب با برنامه زندگی تان شخصی ساز کنید."));
+signUpProcContainer.appendChild(create("../src/img/benefits.png", "مزایا و خدمات باشگاه رانندگان", "در باشگاه رانندگان اسنپ می‌توانید از تسهیلات و خدمات متنوعی از جمله خدمات خودرویی، خدمات درمانی و همچنین خدمات رفاهی و آموزشی بهره‌مند شوید."));
+signUpProcContainer.appendChild(create("../src/img/carfix.png", "کارفیکس", "باشگاه رانندگان اسنپ به‌تازگی سرویس جدید «اسنپ کارفیکس» را برای سهولت دسترسی کاربران راننده به انواع خدمات خودرویی راه‌اندازی کرده است."));
+
+
