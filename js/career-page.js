56044013077da4802ff79be012b47c74678095c8
nima babaei <nimani1378@gmail.com>
2022-09-01 12:50:59 +0430

    add js for automating career input

--- js/career-page.js
+++ js/career-page.js
@@ -8,3 +8,58 @@ $(document).ready(function(){
         variableWidth: true
       });
   });
+
+let cp_careers = [
+    {title:"Associate Product Manager (User)",loc:"Remote job",department:"Product"},
+    {title:"Product Manager",loc:"Tehran, Iran, Islamic Republic of",department:"product"},
+    {title:"Application Security Engineer",loc:"Tehran, Iran, Islamic Republic of",department:"product"},
+    {title:"Penetration Testing Engineer",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Senior Social Media Specialist - SnappPay",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Senior People and Organization Development Specialist",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Legal expert",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Associate Product Manager (User)",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Penetration Testing Engineer",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Application Security Engineer",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Legal expert",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+    {title:"Product Manager",loc:"Tehran, Iran, Islamic Republic of",department:"Product"},
+]
+console.log(cp_careers[0]);
+const cp_careers_grid = document.querySelector('.cp_career-grid');
+cp_careers.forEach(element=>{
+    console.log("hello heree");
+    cp_career_box = document.createElement('a');
+    cp_career_box.classList.add("cp_career-box")
+    let cp_title = document.createElement('h5');
+    cp_title.innerText = element.title;
+    let cp_loc_Dep = document.createElement('div');
+    let cp_loc = document.createElement('span');
+    cp_loc.classList.add("cp_career-location");
+    cp_loc.innerText = element.loc;
+    cp_loc_Dep.appendChild(cp_loc);
+    let cp_Dep = document.createElement('span');
+    cp_Dep.classList.add("cp_career-department");
+    cp_Dep.innerText = element.department;
+    cp_loc_Dep.appendChild(cp_Dep);
+    cp_career_box.appendChild(cp_title);
+    cp_career_box.appendChild(cp_loc_Dep);
+    // let cp_loc_Dep = document.createElement('div');
+    // let cp_loc = document.createElement('span');
+    // cp_loc.classList.add("cp_career-location");
+    // let cp_Dep = document.createElement('span');
+    // cp_Dep.classList.add("cp_career-department");
+    // cp_loc_Dep.appendChild(cp_loc,cp_Dep);
+    // cp_career_box.appendChild(cp_title,cp_loc_Dep)
+    cp_careers_grid.appendChild(cp_career_box)
+})
+
+function myFunction() {
+    const cp_clicked = document.querySelectorAll(".cp_filters");
+    cp_clicked.forEach(element => {
+        element.addEventListener('click', ()=>{
+            cp_clicked.forEach(element1=>{element1.classList.remove("cp_selected")})
+            element.classList.add("cp_selected");
+
+        })
+    });
+}
+myFunction();
