35a3baaf11c26f2c869598eba74d6a68ba4f8d0a
Amir partovi <a.partovi99@gmail.com>
2022-09-01 21:53:11 +0430

    'subMenu'

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,25 @@
 function bl_search_Function() {
-    var x = document.getElementById("bl-search-input");
+    const x = document.getElementById("bl-search-input");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
+    const icon = document.getElementsByClassName(".bl-search-icon")
+    if(icon.classList.contains("fa-solid fa-magnifying-glass")){
+      icon.classList.replace("fa-regular fa-xmark");
+      icon.classList.remove("fa-solid fa-magnifying-glass");
+      icon.classList.add("fa-regular fa-xmark");
+    }else{
+      icon.classList.add("fa-solid fa-magnifying-glass");
+    }
     
   }
+function bl_hamburger_Function(){
+  const x = document.getElementsByClassName(".bl-menu-father");
+  if(x.style.display ==="flex"){
+    x.style.display = "none";
+  }
+  else {
+    x.style.display = "flex";
+}}
