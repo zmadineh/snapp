c27560d30de1f343d559f40f11f220b60e4705e3
Sahar Mir <sahar.mirjahanian.sm@gmail.com>
2022-09-05 23:13:47 -0700

    add possibilities and footer section for main page

--- js/main-page.js
+++ js/main-page.js
@@ -18,6 +18,11 @@ const sendBtn = document.querySelector("#mp-send_btn");
 
 const signUpProcContainer = document.querySelector('#mp-signUp-benefit');
 
+const possibilitiesContainer = document.querySelector('.mp-possibilities-list');
+const showMoreSection = document.querySelector('.mp-show-more');
+const showMoreBtn = document.querySelector('.mp-more_btn');
+const possibilitiesSection = document.querySelector('.mp-possibilities');
+
 function handleHamburgerMneu () {
         hamburger.classList.toggle('active');
         navMneu.classList.toggle('mobileView');
@@ -69,6 +74,41 @@ function create (img, title, body) {
 
         mainContainer.appendChild(image);
         mainContainer.appendChild(container);
+
+        return mainContainer;
+}
+
+function createPossibilities (img, title, body) {
+
+        const mainContainer = document.createElement('div');
+        mainContainer.className = 'mp-possibilities-items';
+
+        const descriptionContainer = document.createElement('div');
+        descriptionContainer.className = 'mp-logo-describes';
+
+        const titleText =  document.createElement('div');
+        titleText.className = 'mp-logotype';
+        titleText.textContent = title;
+
+        const botyText = document.createElement('div');
+        botyText.className = 'mp-logo-moretype';
+        botyText.textContent = body;
+
+        const imgContainer = document.createElement('div');
+        descriptionContainer.className = 'mp-possibilities-logo';
+
+        const image = document.createElement("img");
+        image.className = 'mp-possibilities-image';
+        image.setAttribute('src', img);
+
+
+        descriptionContainer.appendChild(titleText);
+        descriptionContainer.appendChild(botyText);
+
+        imgContainer.appendChild(image);
+
+        mainContainer.appendChild(imgContainer);
+        mainContainer.appendChild(descriptionContainer);
 
         return mainContainer;
 }
@@ -156,12 +196,47 @@ sendBtn.addEventListener("click", () => {
         }
 });
 
+showMoreBtn.addEventListener('click', () => {
+        possibilitiesSection.style.overflow = 'auto';
+        possibilitiesSection.style.height = '100%';
+        
+        showMoreSection.style.display = 'none';
+});
+
 // main program 
 
 signUpProcContainer.appendChild(create("../src/img/main-page/income.png", "درآمد تضمینی + پاداش های ماهانه و هفتگی", "با فعالیت در ناوگان اسنپ، علاوه بر کسب درآمد مستمر و امکان تسویه در لحظه می‌توانید با شرکت در طرح‌های تشویقی مختلف، درآمد خود را افزایش دهید."));
 signUpProcContainer.appendChild(create("../src/img/main-page/hour.png", "ساعت کاری دلخواه", "فعالیت در ناوگان اسنپ محدودیت زمانی ندارد و می توانید فعالیت خود را در هر ساعت از شبانه روز و متناسب با برنامه زندگی تان شخصی ساز کنید."));
 signUpProcContainer.appendChild(create("../src/img/main-page/benefits.png", "مزایا و خدمات باشگاه رانندگان", "در باشگاه رانندگان اسنپ می‌توانید از تسهیلات و خدمات متنوعی از جمله خدمات خودرویی، خدمات درمانی و همچنین خدمات رفاهی و آموزشی بهره‌مند شوید."));
 signUpProcContainer.appendChild(create("../src/img/main-page/carfix.png", "کارفیکس", "باشگاه رانندگان اسنپ به‌تازگی سرویس جدید «اسنپ کارفیکس» را برای سهولت دسترسی کاربران راننده به انواع خدمات خودرویی راه‌اندازی کرده است."));
+
+possibilities = [
+        ["../src/img/main-need-image/cab.svg",'تاکسی اینترنتی', 'امکان درخواست آنلاین خودرو با اسنپ'],
+        ["../src/img/main-need-image/bike.svg",'پیک موتوری','حمل و نقل با اسنپ'],
+        ["../src/img/main-need-image/Pickup-new.png",'درخواست وانت', 'حمل و نقل با اسنپ'],
+        ["../src/img/main-need-image/food.svg",'سفارش آنلاین غذا', 'سفارش غذا، نان و شیرینی با اسنپ'],
+        ["../src/img/main-need-image/market.svg",'سوپرمارکت آنلاین', ' تهیه اقلام روزانه با اسنپ'],
+        ["../src/img/main-need-image/market.svg",'اسنپ اکسپرس', 'تحویل در کمتر از 30 دقیقه'],
+        ["../src/img/main-need-image/trip.svg",'بلیط پرواز داخلی', 'گردشگری با اسنپ'],
+        ["../src/img/main-need-image/trip.svg",'بلیط پرواز خارجی', 'گردشگری با اسنپ'],
+        ["../src/img/main-need-image/doctor.svg",'دکتر', 'درمان با اسنپ'],
+        ["../src/img/main-need-image/hotel.png",'رزرو هتل', 'گردشگری با اسنپ'],
+        ["../src/img/main-need-image/trip.svg",'رزرو اقامتگاه', 'گردشگری با اسنپ'],
+        ["../src/img/main-need-image/new Shop.png",'فروشگاه', 'خرید از فروشگاه های معتبر شهر'],
+        ["../src/img/main-need-image/bus.png",'بلیط اتوبوس', 'گردشگری با اسنپ'],
+        ["../src/img/main-need-image/train.png",'بلیط قطار', 'گردشگری با اسنپ'],
+        ["../src/img/main-need-image/club.svg",'اسنپ! کلاب', 'باشگاه مشتریان اسنپ'],
+        ["../src/img/main-need-image/Service Icon.png",'سرمایه گذاری', 'خدمات غیرحضوری بورس و سرمایه گذاری'],
+        ["../src/img/main-need-image/Moving.png",'اسباب کشی منزل', 'حمل و نقل با اسنپ'],
+        ["../src/img/main-need-image/pay.svg",'سرویس اعتباری', 'پرداخت با اسنپ'],
+        ["../src/img/main-need-image/ticket-icon.svg",'اسنپ تیکت', 'خرید آنلاین بلیط با اسنپ'],
+];
+
+
+
+possibilities.forEach(element => {
+        possibilitiesContainer.appendChild(createPossibilities(element[0], element[1], element[2]));      
+});
 
 let slideIndex = 1;
 showSlides(slideIndex);
